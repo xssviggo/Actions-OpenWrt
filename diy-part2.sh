36f9331fa2190552d9964bf71b33a7a02e586210
892947707 <dswangadmin@163.com>
2021-05-15 16:33:16 +0800

    添加应用过滤插件

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,4 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i "s/OpenWrt /Dswang build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
